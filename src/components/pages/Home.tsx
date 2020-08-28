d8ecd356f1318bdad4a11f16c91aa4c761c68afc
UMASHIBA <umashiba1@gmail.com>
2020-08-28 17:26:58 +0900

    set: cdテスト用コミット

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -11,7 +11,7 @@ function Home() {
     <div className="w-full h-full flex justify-center items-center">
       <div className="w-11/12 h-full shadow-md rounded-md flex justify-center items-center flex-col">
         ホームだよ
-        <Link to="/videopage">ビデオページへGO(やり方1)</Link>
+        <Link to="/videopage">ビデオページへGO(やり方1) lll</Link>
         <button onClick={gotoVideoPage}>ビデオページへGO(やり方2)</button>
       </div>
     </div>
