fc0b0a4f86c176c8268efb6784826d7a80ef574c
UMASHIBA <umashiba1@gmail.com>
2020-09-22 23:56:30 +0900

    mod: Modalの動作確認を追加

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
+import Modal from "../atoms/Modal";
 
 function Home() {
   const history = useHistory();
+
+  const [isOpenModal, changeIsOpenModal] = useState(false);
 
   const gotoVideoPage = () => {
     history.push("/videopage");
   };
   return (
-    <div className="w-full h-full flex justify-center items-center">
-      <div className="w-11/12 h-full shadow-md rounded-md flex justify-center items-center flex-col">
-        ホームだよ
-        <Link to="/videopage">ビデオページへGO(やり方1) lll</Link>
-        <button onClick={gotoVideoPage}>ビデオページへGO(やり方2)</button>
+    <>
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="w-11/12 h-full shadow-md rounded-md flex justify-center items-center flex-col">
+          ホームだよ
+          <Link to="/videopage">ビデオページへGO(やり方1) lll</Link>
+          <button onClick={gotoVideoPage}>ビデオページへGO(やり方2)</button>
+          <Modal isShow={isOpenModal} onClose={() => changeIsOpenModal(false)}>
+            aaa
+          </Modal>
+          <button onClick={() => changeIsOpenModal(true)}>開く</button>
+        </div>
       </div>
-    </div>
+      <div className="h-56 w-full"></div>
+    </>
   );
 }
 
