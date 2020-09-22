fc62976fb65e33e3f814fa4dc59e77a3d7b68972
UMASHIBA <umashiba1@gmail.com>
2020-09-22 22:17:14 +0900

    add: centerPutByPositionを作成

---
+++ src/cssProps/centerPutByPosition.ts
@@ -0,0 +1,10 @@
+import { css } from "styled-components";
+
+const centerPutByPosition = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export default centerPutByPosition;
