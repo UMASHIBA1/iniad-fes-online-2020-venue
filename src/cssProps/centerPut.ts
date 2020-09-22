e429ffbb63f753031dd1912ad961fd4e12e18acd
UMASHIBA <umashiba1@gmail.com>
2020-09-22 21:34:43 +0900

    add: cssPropsを追加

---
+++ src/cssProps/centerPut.ts
@@ -0,0 +1,9 @@
+import { css } from "styled-components";
+
+const centerPut = css`
+  display: grid;
+  justify-items: center;
+  align-items: center;
+`;
+
+export default centerPut;
---
+++ src/cssProps/topLeftZero.ts
@@ -0,0 +1,9 @@
+import { css } from "styled-components";
+
+const topLeftZero = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export default topLeftZero;
