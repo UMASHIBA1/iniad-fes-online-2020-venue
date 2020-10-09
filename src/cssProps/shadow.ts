01eed5419987e9f0b65027c09f52e7cc0058d2cb
UMASHIBA <umashiba1@gmail.com>
2020-10-09 23:35:16 +0900

    mod: shadowのcssPropsを変更

--- src/cssProps/shadow.ts
+++ src/cssProps/shadow.ts
@@ -1,5 +1,6 @@
 import { css } from "styled-components";
+import { shadowProps } from "../constants/colors";
 
 export const normalShadow = (offset = 4) => css`
-  box-shadow: ${offset}px ${offset}px ${offset * 4}px rgba(0, 0, 0, 0.1);
+  box-shadow: ${offset}px ${offset}px ${offset * 4}px ${shadowProps};
 `;
