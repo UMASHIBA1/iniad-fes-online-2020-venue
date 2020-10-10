2917b944a61484a480edbf97d9ca8ec897932e22
UMASHIBA <umashiba1@gmail.com>
2020-10-10 21:51:10 +0900

    mod: useDividedRoomDatasを作成

--- src/hooks/useRoomDatas.ts
+++ src/hooks/useRoomDatas.ts
@@ -1,5 +1,6 @@
+import { useEffect, useState } from "react";
 import roomDatas from "../mockDatas/roomDatas"
-import { RoomAPIType } from "../typings/RoomPropType";
+import { HallProps, HomeProps, RoadProps, RoomAPIType, RoomProps } from "../typings/RoomPropType";
 
 // FIXME: このhooksを書き換えてmockからちゃんとしたAPIへの移行を行う
 const useRoomDatas = (): RoomAPIType => {
@@ -7,3 +8,45 @@ const useRoomDatas = (): RoomAPIType => {
 }
 
 export default useRoomDatas;
+
+
+export const useDividedRoomDatas = () => {
+
+  const divideDatasByRoomType = (roomDatas: RoomAPIType) => {
+    const dividedRoomDatas = {
+      home: [] as HomeProps[],
+      road: [] as RoadProps[],
+      room: [] as RoomProps[],
+      hall: [] as HallProps[],
+    }
+
+    roomDatas.forEach((data) => {
+      switch(data.type) {
+        case "home":
+          dividedRoomDatas.home.push(data);
+          break;
+        case "road":
+          dividedRoomDatas.road.push(data);
+          break;
+        case "room":
+          dividedRoomDatas.room.push(data);
+          break;
+        case "hall":
+          dividedRoomDatas.hall.push(data);
+          break;
+      }
+    });
+
+    return dividedRoomDatas;
+  }
+
+  const roomDatas = useRoomDatas();
+  const [dividedRoomDatas, changeDividedRoomDatas] = useState(divideDatasByRoomType(roomDatas));
+
+  useEffect(() => {
+    changeDividedRoomDatas(divideDatasByRoomType(roomDatas));
+  }, [roomDatas]);
+
+  return [dividedRoomDatas];
+
+}
