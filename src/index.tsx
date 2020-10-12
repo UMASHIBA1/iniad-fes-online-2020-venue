7afb752fb69d83d222dd5dcee88431dcba8c65df
yuma <s1f101803219@iniad.org>
2020-10-13 05:47:12 +0900

    add: reduxの導入完了

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,14 @@ import * as serviceWorker from "./serviceWorker";
 import "./styles/tailwind.compiled.css";
 import "./styles/global.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
