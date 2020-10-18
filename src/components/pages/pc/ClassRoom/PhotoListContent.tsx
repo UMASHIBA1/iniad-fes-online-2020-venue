efdfa2519abaac156e4a227acd47976d250d6632
UMASHIBA <umashiba1@gmail.com>
2020-10-18 22:05:42 +0900

    mod: pc版のPhotoLIstCOntentを作成

--- src/components/pages/pc/ClassRoom/PhotoListContent.tsx
+++ src/components/pages/pc/ClassRoom/PhotoListContent.tsx
@@ -3,28 +3,48 @@ import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 import { RoomUrlType } from "../../../../constants/links";
 import { PhotoListEnvAttr } from "../../../../typings/RoomPropType/ClassRoomProps";
+import RoomMark from "../../../atoms/RoomMark";
 import PhotoListModal from "../../../organisms/PhotoListModal";
+import logoPath from "../../../../statics/svgs/iniadfes-logo.svg";
+import ObjectMark from "../../../atoms/ObjectMark";
 
 interface Props {
   photoListEnvProps: PhotoListEnvAttr;
   history: ReturnType<typeof useHistory>;
 }
 
-function PhotoListContent({photoListEnvProps, history}: Props) {
-    const [isShowModal, changeIsShowModal] = useState(true);
+const dataControllId = {
+  objButton: "photolistRoomContent-obj-button",
+  door: "photolistRoomContent-left-door",
+};
+
+function PhotoListContent({ photoListEnvProps, history }: Props) {
+  const [isShowModal, changeIsShowModal] = useState(false);
   const gotoTargetUrl = (url: RoomUrlType) => {
     history.push(url);
   };
-  return(
+  return (
     <Wrapper>
       PhotoListContent
+      <RoomMark
+        imgPath={logoPath}
+        dataControllId={dataControllId.door}
+        roomTitle={photoListEnvProps.door.title}
+        onClick={() => {
+          gotoTargetUrl(photoListEnvProps.door.url);
+        }}
+      />
+      <ObjectMark
+        onClick={() => changeIsShowModal(true)}
+        dataControllId={dataControllId.objButton}
+      />
       <PhotoListModal
-      isShow={isShowModal}
+        isShow={isShowModal}
         onClose={() => changeIsShowModal(false)}
         photoIframes={photoListEnvProps.photoIframes}
         title={photoListEnvProps.title}
         description={photoListEnvProps.description}
-        />
+      />
     </Wrapper>
   );
 }
@@ -35,6 +55,19 @@ const Wrapper = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-`
+
+  > button {
+    &[data-controll-id=${dataControllId.door}] {
+      position: absolute;
+      top: 60%;
+      left: 20%;
+    }
+
+    &[data-controll-id=${dataControllId.objButton}] {
+      position: absolute;
+      top: 50%;
+      left: 45%;
+    }
+  }`
 
 export default PhotoListContent;
