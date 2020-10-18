e6a010f12a159014196cad55b5a70da1b4ef005d
UMASHIBA <umashiba1@gmail.com>
2020-10-18 20:02:32 +0900

    add: PhotoListContentの原型作成

---
+++ src/components/pages/mobile/ClassRoom/PhotoListContent.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+
+function PhotoListContent() {
+  return(
+    <Wrapper>
+      PhotoListContent
+      {/*  */}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+    position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`
+
+export default PhotoListContent;
