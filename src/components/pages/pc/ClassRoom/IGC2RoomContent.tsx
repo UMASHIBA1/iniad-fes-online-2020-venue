e322cc2b983e6e3d2204b3915c3724461af17448
UMASHIBA <umashiba1@gmail.com>
2020-10-22 08:27:51 +0900

    pc版のIGC2の教室変更

--- src/components/pages/pc/ClassRoom/IGC2RoomContent.tsx
+++ src/components/pages/pc/ClassRoom/IGC2RoomContent.tsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
 import { RoomUrlType } from "../../../../constants/links";
 import { IGC2EnvAttr } from "../../../../typings/RoomPropType/ClassRoomProps";
 import RoomMark from "../../../atoms/RoomMark";
-import ObjectMark from "../../../atoms/ObjectMark";
 import styled from "styled-components";
 import { whiteText } from "../../../../cssProps/colors";
 import { radiusLg, radiusMd } from "../../../../cssProps/radius";
 import breakPoints from "../../../../constants/breakPoints";
 import Img from "../../../atoms/Img";
 import Link from "../../../atoms/Link";
-import VideoModal from "../../../molecules/VideoModal";
+import ClassRoomVideo from "../../../organisms/ClassRoomVideo";
+import { normalShadow } from "../../../../cssProps/shadow";
 
 interface Props {
   igc2EnvProps: IGC2EnvAttr;
@@ -23,7 +23,6 @@ const dataControllId = {
 };
 
 function IGC2RoomContent({ igc2EnvProps: env, history }: Props) {
-  const [isShowModal, changeIsShowModal] = useState(false);
   const gotoTargetUrl = (url: RoomUrlType) => {
     history.push(url);
   };
@@ -38,20 +37,7 @@ function IGC2RoomContent({ igc2EnvProps: env, history }: Props) {
           gotoTargetUrl(env.door.url);
         }}
       />
-      <ObjectMark
-      title="動画"
-        onClick={() => changeIsShowModal(true)}
-        dataControllId={dataControllId.objButton}
-      />
-      <VideoModal
-        isMobile={false}
-        isShow={isShowModal}
-        onClose={() => {
-          changeIsShowModal(false);
-        }}
-        title={env.title}
-        videoPropList={[env.video]}
-      />
+      <ClassRoomVideo videoProps={env.video} />
       <LinkWrapper>
         <Link link={env.gameLink}>
           <ImgWrapper>
@@ -74,8 +60,8 @@ const Wrapper = styled.div`
   > button {
     &[data-controll-id=${dataControllId.door}] {
       position: absolute;
-      top: 60%;
-      left: 20%;
+      top: 47%;
+      left: 5%;
     }
 
     &[data-controll-id=${dataControllId.objButton}] {
@@ -105,8 +91,8 @@ const LinkWrapper = styled.div`
   width: 160px;
   ${radiusMd}
   position: absolute;
-  top: 35%;
-  right: 20%;
+  top: 31%;
+  right: 31%;
 `;
 
 const ImgWrapper = styled.div`
@@ -120,6 +106,7 @@ const ImgWrapper = styled.div`
   transition: filter 200ms ease-in;
   ${radiusLg}
   margin-bottom: 16px;
+  ${normalShadow(6)}
   filter: blur(0px);
   :hover {
     filter: blur(2px);
