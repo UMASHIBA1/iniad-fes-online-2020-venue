369eccd605976b269ae51ec1639fe65fd9e19446
UMASHIBA <umashiba1@gmail.com>
2020-10-27 04:29:48 +0900

    mod: EscapeGameQuestionにmultiSelectの表示機能を追加

--- src/components/molecules/EscapeGameQuestionModal.tsx
+++ src/components/molecules/EscapeGameQuestionModal.tsx
@@ -4,6 +4,7 @@ import EscapeGameQuestion from "../../typings/EscapeGame/EscapeGameQuestion";
 import { AnswerSelection } from "../../typings/EscapeGame/EscapeGameUserInfo";
 import ViewingProp from "../../typings/ViewingProp";
 import Button from "../atoms/Button/Button";
+import Checkboxs from "../atoms/Checkboxs";
 import Modal from "../atoms/Modal/Modal";
 import Select from "../atoms/RadioSelect";
 import TextInput from "../atoms/TextInput";
@@ -14,7 +15,8 @@ interface Props {
   viewing?: ViewingProp;
   isMobile: boolean;
   escapeGameProps: EscapeGameQuestion;
-  onSubmit: (textInput: string | AnswerSelection) => void;
+  onSubmit?: (result: string | AnswerSelection) => void;
+  onSubmitMulti?: (result: AnswerSelection[]) => void;
 }
 
 function EscapeGameQuestionModal({
@@ -23,10 +25,16 @@ function EscapeGameQuestionModal({
   viewing = "left",
   isMobile,
   escapeGameProps,
-  onSubmit
+  onSubmit,
+  onSubmitMulti
 }: Props) {
   const [textValue, changeTextValue] = useState("");
-  const [selectValue, changeSelectValue] = useState<AnswerSelection | null>(null);
+  const [selectValue, changeSelectValue] = useState<AnswerSelection | null>(
+    null
+  );
+  const [selectedValueList, changeSelectedValueList] = useState<
+    AnswerSelection[]
+  >([]);
 
   return (
     <Modal
@@ -38,10 +46,12 @@ function EscapeGameQuestionModal({
       <Wrapper>
         <Img src={escapeGameProps.questionImg} alt={escapeGameProps.title} />
         {escapeGameProps.mode === "text" ? (
-          <Form onSubmit={(e) => {
-            onSubmit(textValue);
-            e.preventDefault();
-            }}>
+          <Form
+            onSubmit={(e) => {
+              onSubmit&&onSubmit(textValue);
+              e.preventDefault();
+            }}
+          >
             <TextInput
               required={true}
               name={escapeGameProps.title}
@@ -52,37 +62,76 @@ function EscapeGameQuestionModal({
             <Button text="提出" mode="blue" type="submit" useShadow={false} />
           </Form>
         ) : null}
-        {
-          escapeGameProps.mode === "selectThree"? (
-            <Form onSubmit={(e) => {
-              if(selectValue !== null) {
-                onSubmit(selectValue);
+        {escapeGameProps.mode === "selectThree" ? (
+          <Form
+            onSubmit={(e) => {
+              if (selectValue !== null) {
+                onSubmit&&onSubmit(selectValue);
               }
               e.preventDefault();
-            }}>
-              <Select
+            }}
+          >
+            <Select
               name={escapeGameProps.title}
-              value={selectValue?selectValue: ""}
-              onChange={(value) => {value !== "" && changeSelectValue(value as AnswerSelection)}}
+              value={selectValue ? selectValue : ""}
+              onChange={(value) => {
+                value !== "" && changeSelectValue(value as AnswerSelection);
+              }}
               optionList={[
                 {
                   label: "1",
-                  value: "1"
+                  value: "1",
                 },
                 {
                   label: "2",
-                  value: "2"
+                  value: "2",
                 },
                 {
                   label: "3",
-                  value: "3"
-                }
+                  value: "3",
+                },
               ]}
-              />
+            />
             <Button text="提出" mode="blue" type="submit" useShadow={false} />
-            </Form>
-          ): null
-        }
+          </Form>
+        ) : null}
+        {escapeGameProps.mode === "multiSelect" ? (
+          <Form
+            onSubmit={(e) => {
+              if (selectedValueList.length > 0) {
+                console.log("runsubmit");
+                onSubmitMulti&&onSubmitMulti(selectedValueList);
+              }
+              e.preventDefault();
+            }}
+          >
+            <Checkboxs
+              onChange={(selectedList) => {
+                changeSelectedValueList(selectedList as AnswerSelection[]);
+              }}
+              selected={selectedValueList as string[]}
+              selectableOptions={[
+                {
+                  label: "1",
+                  value: "1",
+                },
+                {
+                  label: "2",
+                  value: "2",
+                },
+                {
+                  label: "3",
+                  value: "3",
+                },
+                {
+                  label: "4",
+                  value: "4",
+                },
+              ]}
+            />
+            <Button text="提出" mode="blue" type="submit" useShadow={false} />
+          </Form>
+        ) : null}
       </Wrapper>
     </Modal>
   );
