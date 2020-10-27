430b2eb93a259c02da7f168a2d5ed191db2d58e1
UMASHIBA <umashiba1@gmail.com>
2020-10-28 02:58:19 +0900

    mod: artWorksのデータを本物に変更

--- src/mockDatas/roomDatas.ts
+++ src/mockDatas/roomDatas.ts
@@ -701,28 +701,56 @@ const roomDatas: RoomAPIType = [
       mode: "artList",
       artList: [
         {
-          title: "なんか",
-          url: iniadTutorialIcon,
+          title: "冨澤 作",
+          url:
+            "https://storage.googleapis.com/iniadfes/public/ArtWorks/tomizawa1.png",
         },
         {
-          title: "nannka",
-          url: raisonDetleIcon,
+          title: "冨澤 作",
+          url:
+            "https://storage.googleapis.com/iniadfes/public/ArtWorks/tomizawa2.png",
         },
         {
-          title: "nannkadayo",
-          url: quizIcon,
+          title: "たさき 作",
+          url:
+            "https://storage.googleapis.com/iniadfes/public/ArtWorks/tasaki.png",
         },
         {
-          title: "doudesyouka1",
-          url: iniadfesLogoIcon,
+          title: "河西 作",
+          url:
+            "https://storage.googleapis.com/iniadfes/public/ArtWorks/kanishi.jpg",
         },
         {
-          title: "doudesyouka2",
-          url: iniadfesLogoIcon,
+          title: "高橋 作",
+          url:
+            "https://storage.googleapis.com/iniadfes/public/ArtWorks/takahashi.png",
         },
         {
-          title: "doudesyouka3",
-          url: iniadfesLogoIcon,
+          title: "作田 作",
+          url:
+            "https://storage.googleapis.com/iniadfes/public/ArtWorks/sakuta1.png",
+        },
+        {
+          title: "作田 作",
+          url:
+            "https://storage.googleapis.com/iniadfes/public/ArtWorks/sakuta2.png",
+        },
+        {
+          title: "作田 作",
+          url:
+            "https://storage.googleapis.com/iniadfes/public/ArtWorks/sakuta3.png",
+        },
+        {
+          title: "小田 作",
+          url: "https://storage.googleapis.com/iniadfes/public/ArtWorks/koda.png",
+        },
+                {
+          title: "小田 作",
+          url: "https://storage.googleapis.com/iniadfes/public/ArtWorks/koda2.PNG",
+        },
+                {
+          title: "渡邉 作",
+          url: "https://storage.googleapis.com/iniadfes/public/ArtWorks/watanabe.png",
         },
       ],
       description: "Art Worksの書いてきた絵です",
