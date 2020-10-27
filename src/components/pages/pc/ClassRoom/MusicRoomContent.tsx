4d8067a5676d6c01cb37e805818a258525aea529
UMASHIBA <umashiba1@gmail.com>
2020-10-27 20:45:39 +0900

    mod: MusicRoomContentに4年間の結果の表示モーダルを作成

--- src/components/pages/pc/ClassRoom/MusicRoomContent.tsx
+++ src/components/pages/pc/ClassRoom/MusicRoomContent.tsx
@@ -6,12 +6,12 @@ import MusicModal from "../../../organisms/MusicModal";
 import { useHistory } from "react-router-dom";
 import { RoomUrlType } from "../../../../constants/links";
 import styled from "styled-components";
-import useDidMount from "../../../../hooks/useDidMount/useDidMount";
 import EscapeGameUserInfo from "../../../../typings/EscapeGame/EscapeGameUserInfo";
 import { useDispatch } from "react-redux";
 import { DispatchType, useTypedSelector } from "../../../../redux/store";
 import EscapeGameQuestionModal from "../../../molecules/EscapeGameQuestionModal";
-import { answerQ4, incrementGrade } from "../../../../redux/modules/escapeGameUserInfo";
+import { answerQ4 } from "../../../../redux/modules/escapeGameUserInfo";
+import FinishEscapeGameModal from "../../../organisms/FinishEscapeGameModal";
 
 interface Props {
   musicEnvProps: MusicEnvAttr;
@@ -24,30 +24,35 @@ const dataControllId = {
   escapeGameButton: "musicRoomContent-escapegame-button",
 };
 
-
-const judgeUsersCourseProps = (course: EscapeGameUserInfo["course"], musicEnvProps: MusicEnvAttr) => {
-  if(course === "engineer" && musicEnvProps.engEscapeGameQuestion) {
+const judgeUsersCourseProps = (
+  course: EscapeGameUserInfo["course"],
+  musicEnvProps: MusicEnvAttr
+) => {
+  if (course === "engineer" && musicEnvProps.engEscapeGameQuestion) {
     return musicEnvProps.engEscapeGameQuestion;
-  }else if(course === "design" && musicEnvProps.designEscapeGameQuestion) {
+  } else if (course === "design" && musicEnvProps.designEscapeGameQuestion) {
     return musicEnvProps.designEscapeGameQuestion;
-  }else if(course === "business" && musicEnvProps.busiEscapeGameQuestion) {
+  } else if (course === "business" && musicEnvProps.busiEscapeGameQuestion) {
     return musicEnvProps.busiEscapeGameQuestion;
-  }else if(course==="civil" && musicEnvProps.civilEscapeGameQuetion) {
+  } else if (course === "civil" && musicEnvProps.civilEscapeGameQuetion) {
     return musicEnvProps.civilEscapeGameQuetion;
   } else {
     return null;
   }
-}
+};
 
 function MusicRoomContent({ musicEnvProps, history }: Props) {
   const [isShowModal, changeIsShowModal] = useState(false);
   const [isShowQuestionModal, changeIsShowQuestionModal] = useState(false);
+  const [isShowFinishGameModal, changeIsShowFinishGameModal] = useState(false);
   const gotoTargetUrl = (url: RoomUrlType) => {
     history.push(url);
   };
 
   const dispatch: DispatchType = useDispatch();
-  const {userAnswer, course} = useTypedSelector(({escapeGameUserInfo}) => escapeGameUserInfo);
+  const { userAnswer, course } = useTypedSelector(
+    ({ escapeGameUserInfo }) => escapeGameUserInfo
+  );
   const q4Answer = userAnswer.q4;
 
   const usersCourseQuestion = judgeUsersCourseProps(course, musicEnvProps);
@@ -62,7 +67,11 @@ function MusicRoomContent({ musicEnvProps, history }: Props) {
           gotoTargetUrl(musicEnvProps.door.url);
         }}
       />
-      <ObjectMark title="音楽" onClick={() => changeIsShowModal(true)} dataControllId={dataControllId.objButton} />
+      <ObjectMark
+        title="音楽"
+        onClick={() => changeIsShowModal(true)}
+        dataControllId={dataControllId.objButton}
+      />
       <MusicModal
         isShow={isShowModal}
         onClose={() => changeIsShowModal(false)}
@@ -71,16 +80,24 @@ function MusicRoomContent({ musicEnvProps, history }: Props) {
         pickupMusics={musicEnvProps.pickUpIframes}
         isMobile={false}
       />
-      {usersCourseQuestion?(
+      {usersCourseQuestion ? (
         <React.Fragment>
-          {q4Answer === null &&(
-          <ObjectMark
-            color="blue"
-            title={usersCourseQuestion.title}
-            onClick={() => changeIsShowQuestionModal(true)}
-            dataControllId={dataControllId.escapeGameButton}
-          />
+          {q4Answer === null && (
+            <ObjectMark
+              color="blue"
+              title={usersCourseQuestion.title}
+              onClick={() => changeIsShowQuestionModal(true)}
+              dataControllId={dataControllId.escapeGameButton}
+            />
           )}
+          {q4Answer !== null ? (
+            <ObjectMark
+              color="blue"
+              title="4年間の成績"
+              onClick={() => changeIsShowFinishGameModal(true)}
+              dataControllId={dataControllId.escapeGameButton}
+            />
+          ) : null}
           <EscapeGameQuestionModal
             escapeGameProps={usersCourseQuestion}
             isMobile={false}
@@ -89,7 +106,7 @@ function MusicRoomContent({ musicEnvProps, history }: Props) {
             onSubmit={(str) => {
               dispatch(answerQ4(str));
               changeIsShowQuestionModal(false);
-              alert("問題4の答えを受け取ったよ！")
+              alert("問題4の答えを受け取ったよ！");
             }}
             onSubmitMulti={(list1) => {
               dispatch(answerQ4(list1));
@@ -97,8 +114,15 @@ function MusicRoomContent({ musicEnvProps, history }: Props) {
               alert("問題4の答えを受け取ったよ！");
             }}
           />
+          <FinishEscapeGameModal
+            isMobile={false}
+            isShow={isShowFinishGameModal}
+            onClose={() => {
+              changeIsShowFinishGameModal(false);
+            }}
+          />
         </React.Fragment>
-      ): null}
+      ) : null}
     </Wrapper>
   );
 }
@@ -124,7 +148,7 @@ const Wrapper = styled.div`
       left: 33%;
     }
 
-        &[data-controll-id=${dataControllId.escapeGameButton}] {
+    &[data-controll-id=${dataControllId.escapeGameButton}] {
       position: absolute;
       top: 28%;
       right: 33%;
