341ebd43d2ea4edd6fb007330bdf2eb0505ea106
UMASHIBA <umashiba1@gmail.com>
2020-10-28 01:41:06 +0900

    add: pc版のyoutubeRoomを作成

---
+++ src/components/pages/pc/ClassRoom/YouTubeRoomContent.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { RoomUrlType } from "../../../../constants/links";
+import { OneVideoEnvAttr, YoutubeEnvAttr } from "../../../../typings/RoomPropType/ClassRoomProps";
+import RoomMark from "../../../atoms/RoomMark";
+import styled, { css } from "styled-components";
+import YoutubeEmbed from "../../../organisms/YoutubeEmbed";
+
+interface Props {
+  youtubeRoomProps: YoutubeEnvAttr;
+  history: ReturnType<typeof useHistory>;
+}
+
+const dataControllId = {
+  door: "oneVideoRoomContent-left-door",
+};
+
+function YouTubeRoomContent({ youtubeRoomProps: env, history }: Props) {
+  const gotoTargetUrl = (url: RoomUrlType) => {
+    history.push(url);
+  };
+  return (
+    <Wrapper leftOrRight={env.leftOrRight}>
+      <RoomMark
+        imgPath={env.door.imgPath}
+        dataControllId={dataControllId.door}
+        roomTitle={env.door.title}
+        onClick={() => {
+          gotoTargetUrl(env.door.url);
+        }}
+      />
+      <YoutubeEmbed
+      youtubeIframeLink={env.youtubelink}
+      />
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div<{ leftOrRight: OneVideoEnvAttr["leftOrRight"] }>`
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  > button {
+    ${({ leftOrRight }) =>
+      leftOrRight === "left" &&
+      css`
+        &[data-controll-id=${dataControllId.door}] {
+          position: absolute;
+          top: 40%;
+          right: 3%;
+        }
+      `}
+
+    ${({ leftOrRight }) =>
+      leftOrRight === "right" &&
+      css`
+        &[data-controll-id=${dataControllId.door}] {
+          position: absolute;
+          top: 40%;
+          left: 3%;
+        }
+      `}
+  }
+`;
+
+export default YouTubeRoomContent;
