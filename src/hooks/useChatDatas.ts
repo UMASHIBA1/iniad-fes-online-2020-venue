48140c85208e961eb188aa7882128c9351f51390
UMASHIBA <umashiba1@gmail.com>
2020-10-29 14:50:13 +0900

    mod: チャットを受信した時履歴が消えてしまう問題を修正

--- src/hooks/useChatDatas.ts
+++ src/hooks/useChatDatas.ts
@@ -1,13 +1,17 @@
 import { chatRoomChannel } from "../constants/websocketParams";
 import ChatType, { ChatAPIType, ChatPostType } from "../typings/ChatType";
 import ChannelController from "../utils/ChannelController";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useDidMount from "./useDidMount/useDidMount";
 import getChatHistory from "../utils/getChatHistory";
 
 const useChatDatas = (roomId: string) => {
   const [chatDatas, changeChatDatas] = useState<ChatType[]>([]);
   const [cable, changeCable] = useState<ChannelController<ChatAPIType, ChatPostType> | null>(null);
+
+  const changeDataByReceiveNewChat = (data: ChatAPIType) => {
+    changeChatDatas([...chatDatas, data.payload]);
+  }
 
   useDidMount(() => {
     getChatHistory(roomId)
@@ -30,22 +34,25 @@ const useChatDatas = (roomId: string) => {
         ]);
       });
 
-    const chatRoomParams = {
+
+
+    const cable = new ChannelController<ChatAPIType, ChatPostType>();
+    changeCable(cable);
+  });
+
+  useEffect(() => {
+        const chatRoomParams = {
       channel: chatRoomChannel,
       room_id: roomId,
     };
 
-    const cable = new ChannelController<ChatAPIType, ChatPostType>();
-    cable.connect(chatRoomParams, {
-      onReceive: (data) => {
-        changeChatDatas([...chatDatas, data.payload]);
-      },
+        cable&&cable.connect(chatRoomParams, {
+      onReceive: changeDataByReceiveNewChat,
       onConnected: () => {
         // cable.send({ payload: { text: "どうだろうか4", room_id:roomId }, type: "chat" });
       },
     });
-    changeCable(cable);
-  });
+  }, [chatDatas]);
 
   return {
     chatDatas,
