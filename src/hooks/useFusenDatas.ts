e9f4e2fa46705c8bb613be1016c26e3e27017b3b
UMASHIBA <umashiba1@gmail.com>
2020-10-31 04:55:58 +0900

    add: useFusenDatasを作成

---
+++ src/hooks/useFusenDatas.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import { useTypedSelector } from "../redux/store";
+import getChatHistory from "../utils/getChatHistory";
+
+const useFusenDatas = () => {
+  const [fusenTexts, changeFusenTexts] = useState<string[]>([]);
+  const fusenRoomId = useTypedSelector((state) => state.fusenRoomId);
+
+  useEffect(() => {
+    getChatHistory(fusenRoomId).then((fusenData) => {
+      const texts = [];
+      for (const data of fusenData.data.payload.history) {
+        texts.push(data.text);
+      }
+      changeFusenTexts(texts);
+    });
+  }, [fusenRoomId]);
+
+  return fusenTexts;
+}
+
+export default useFusenDatas;
